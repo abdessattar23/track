05dedeca462b97e6a596f98ce2cfcb003b894251
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-06-01 23:28:55 +0200

    Update write.js

--- .netlify/functions/write.js
+++ .netlify/functions/write.js
@@ -1,6 +1,6 @@
 exports.handler = async function(event) {
   const fs = require('fs');
-  const data = JSON.parse(fs.readFileSync('data.json'));
+  const data = JSON.parse(fs.readFileSync('../../data.json'));
   const dateObj = new Date();
   const time = dateObj.toISOString();
   const new_id = Object.keys(data).length;
@@ -11,7 +11,7 @@ exports.handler = async function(event) {
     "access": time
   };
   data[new_id] = new_entry;
-  fs.writeFileSync('data.json', JSON.stringify(data));
+  fs.writeFileSync('../../data.json', JSON.stringify(data));
   return {
     statusCode: 200,
     body: JSON.stringify(new_entry)
